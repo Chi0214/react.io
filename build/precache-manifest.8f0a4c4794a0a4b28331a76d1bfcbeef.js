f6118e13f187d9c7aee7633c02e320c862482bb4
255177826@qq.com <255177826@qq.com>
2020-10-18 20:25:29 +0800

    build

---
+++ build/precache-manifest.8f0a4c4794a0a4b28331a76d1bfcbeef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b2c65252c23212b862ea8f4b5b5bd98",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f311fc58e23796ec7a21",
+    "url": "/static/css/main.95667317.chunk.css"
+  },
+  {
+    "revision": "00e9c819eae81a23aa37",
+    "url": "/static/js/2.4dd10e17.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.4dd10e17.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f311fc58e23796ec7a21",
+    "url": "/static/js/main.43179310.chunk.js"
+  },
+  {
+    "revision": "312ce6a34b2b8e3c74e4",
+    "url": "/static/js/runtime-main.8b4d3552.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
