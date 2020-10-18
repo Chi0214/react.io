27d1aa177eba8bb8181fc1ede555588fea519345
Chi0214 <37214061+Chi0214@users.noreply.github.com>
2020-10-18 20:21:32 +0800

    App.js

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import RouterList from './router/router'
+function App() {
+  return (
+    <div className="App">
+        <RouterList />
+    </div>
+  );
+}
+
+export default App;
